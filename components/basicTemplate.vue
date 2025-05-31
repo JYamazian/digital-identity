c81d0686d93b99f8bff7d5a7d28eef46ceea7959
jyamazian <jeanyamazian@outlook.com>
2025-05-31 11:34:06 +0300

    Fix basic template - dark/light modes + imageCover validation

--- components/basicTemplate.vue
+++ components/basicTemplate.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="min-h-screen flex flex-col items-center justify-center text-center p-6 bg-gray-50 text-gray-800 dark:bg-neutral-800 dark:text-white"
         :style="backgroundStyle">
-        <div class="w-full max-w-3xl rounded-2xl shadow-xl p-8 bg-white/10 dark:bg-black/10 backdrop-blur-md backdrop-saturate-150 border border-white/20 dark:border-white/10 text-white dark:text-gray-200 flex flex-col items-center justify-center text-center gap-6">
+        <div class="flex flex-col items-center justify-center text-center gap-6" :class="{
+            'w-full max-w-3xl rounded-2xl shadow-xl p-8 border border-white/20 dark:border-white/10 bg-white/10 dark:bg-black/10 backdrop-blur-md backdrop-saturate-150 text-white dark:text-gray-200': config.imageCover,
+            '': !config.imageCover // No additional classes when imageCover is not set
+        }">
 
             <Head>
                 <Title>{{ config.siteTitle }}</Title>
@@ -19,17 +22,17 @@
 
             <div class="flex flex-wrap justify-center gap-4 mb-6 w-full max-w-lg">
                 <a v-for="(link, i) in parsedLinks" :key="i" :href="link.url"
-                    class="flex items-center justify-center gap-2 px-3 py-3 border-2 border-gray-300 rounded-full shadow hover:bg-gray-100 transition dark:text-neutral-200 dark:hover:bg-neutral-700 dark:border-neutral-700 dark:shadow-[0_0_6px_rgba(255,255,255,0.08)]"
+                    class="flex items-center justify-center gap-2 px-3 py-3 border-2 border-neutral-300 rounded-full shadow hover:bg-gray-100 transition dark:text-neutral-200 dark:hover:bg-neutral-300 dark:border-neutral-700 dark:shadow-[0_0_6px_rgba(255,255,255,0.08)]"
                     target="_blank" rel="noopener noreferrer">
                     <img :src="link.icon" :alt="link.label" class="w-6 h-6" />
                 </a>
             </div>
             <button @click="generateVCard"
-                class="flex items-center justify-center gap-2 px-4 py-2 mb-6 border-2 border-gray-300 rounded-full shadow hover:bg-gray-100 transition dark:text-neutral-200 dark:hover:bg-neutral-700 dark:border-neutral-700">
+                class="flex items-center justify-center gap-2 px-4 py-2 mb-6 border-2 border-gray-300 rounded-full shadow hover:bg-gray-100 transition dark:text-neutral-200 dark:hover:bg-neutral-300 dark:hover:text-gray-700 dark:border-neutral-700">
                 Contact (.vcf)
             </button>
 
-            <footer class="text-sm text-gray-500 mt-10 dark:text-neutral-200">
+            <footer class="text-sm mt-10 dark:text-neutral-200">
                 {{ config.copyright }}
             </footer>
         </div>
@@ -44,5 +47,7 @@ const { generateVCard } = useVCard()
 const { backgroundStyle } = useCoverCompute()
 
 const config = useRuntimeConfig().public
-const parsedLinks = JSON.parse(String(config.links)) 
+const parsedLinks = Array.isArray(config.links)
+  ? config.links.filter((link: any) => typeof link === 'object' && link !== null && 'icon' in link && 'label' in link && 'url' in link)
+  : []
 </script>
